b1de7bb6034eafd8591980b8c1d4f91983f4ffdf
Natalie Rivera <natalierivera@Natalies-MacBook-Air.local>
2021-04-04 17:59:35 -0600

    started to add functionality to dynamically change the color of the time segments based on the time slot of the event and the current time of the day

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,28 +1,26 @@
-// //hello 
-// <div class="row" >
-//         <div class=" col-2 hour "><span > 10am</span></div>
-//         <div class="col-8 description past"></div>
-//         <button class="saveBtn col-2"></button>
-//       </div>
 
 //create each hour segment for the work day 
 function createHourSegments(startTime, endTime){
+    
 
     while(startTime <= endTime){
+        var mom = moment(startTime, 'HH:SS');
 
         //create a div element with class row
         var row = $("<div>").addClass("row");
 
         var hourDiv = $("<div>").addClass("col-2 hour");
         //create the element to display the hour 
-        var hourSegment = $("<span>").text(startTime);
-
+        var hourSegment = $("<span>").text(mom.format("HH:SS"));
+        
         hourDiv.append(hourSegment);
-
+        
         //create the discription area for the event
-        var descriptionDiv = $("<div>").addClass("description col-8 ");
+        var descriptionDiv = $("<div>").addClass("description col-8");
         var textArea = $("<textarea>").addClass("textarea");
 
+        checkHourSegment(hourSegment, textArea);
+        
         descriptionDiv.append(textArea);
 
         //create the button that saves the event 
@@ -33,5 +31,25 @@ function createHourSegments(startTime, endTime){
         startTime++;
     }
 }
+
+
+//check to see if the event is in the past, 
+//current, or future and update the color of the textarea
+function checkHourSegment(timeEl, textAreaEl){
+    var currTime = moment().format("LT");
+    var time = timeEl.text();
+    console.log(moment(time, "hh").isAfter(currTime, "hour"));
+    if (moment(currTime).isAfter(time)){
+        console.log("is after");
+        $(textAreaEl).addClass("past");
+    } 
+
+}
+
+
+
+//when the save button is pressed the inputed 
+//text by the user is saved in the textarea
+
 
 createHourSegments(09, 17);
