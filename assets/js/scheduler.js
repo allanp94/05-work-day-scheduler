b5e4a937da22c726fa5021ca1ea7d3af0f070d4e
allan.pirillis <allan.p94@gmail.com>
2021-04-04 23:09:21 -0500

    added functionality for the events to update every hour; added the save icon to the button

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,55 +1,83 @@
+var events = [];
 
-//create each hour segment for the work day 
-function createHourSegments(startTime, endTime){
-    
+//create each hour segment for the work day
+function createHourSegments(startTime, endTime) {
+  while (startTime <= endTime) {
+    var mom = moment(startTime, "HH:SS");
 
-    while(startTime <= endTime){
-        var mom = moment(startTime, 'HH:SS');
+    //create a div element with class row
+    var row = $("<div>").addClass("row");
+    row.attr("time", startTime);
 
-        //create a div element with class row
-        var row = $("<div>").addClass("row");
+    var hourDiv = $("<div>").addClass("col-2 hour");
+    //create the element to display the hour
+    var hourSegment = $("<span>").text(mom.format("HH:SS"));
 
-        var hourDiv = $("<div>").addClass("col-2 hour");
-        //create the element to display the hour 
-        var hourSegment = $("<span>").text(mom.format("HH:SS"));
-        
-        hourDiv.append(hourSegment);
-        
-        //create the discription area for the event
-        var descriptionDiv = $("<div>").addClass("description col-8");
-        var textArea = $("<textarea>").addClass("textarea");
+    hourDiv.append(hourSegment);
 
-        checkHourSegment(hourSegment, textArea);
-        
-        descriptionDiv.append(textArea);
+    //create the discription area for the event
+    var descriptionDiv = $("<div>").addClass("description col-8");
+    var textArea = $("<textarea>").addClass("textarea");
 
-        //create the button that saves the event 
-        var saveButton = $("<button>").addClass("saveBtn col-2");
+    descriptionDiv.append(textArea);
 
-        row.append(hourDiv, descriptionDiv, saveButton);
-        $(".container").append(row);
-        startTime++;
-    }
+    //create the button that saves the event
+    var saveButton = $("<button>").addClass("saveBtn col-2 btn btn-default");
+    //   glyphicon glyphicon - floppy - save
+
+    var saveIcon = $("<i>").addClass("fas fa-save");
+
+    saveButton.append(saveIcon);
+
+    row.append(hourDiv, descriptionDiv, saveButton);
+    checkHourSegment(row);
+    $(".container").append(row);
+    startTime++;
+  }
 }
 
-
-//check to see if the event is in the past, 
+//check to see if the event is in the past,
 //current, or future and update the color of the textarea
-function checkHourSegment(timeEl, textAreaEl){
-    var currTime = moment().format("LT");
-    var time = timeEl.text();
-    console.log(moment(time, "hh").isAfter(currTime, "hour"));
-    if (moment(currTime).isAfter(time)){
-        console.log("is after");
-        $(textAreaEl).addClass("past");
-    } 
+function checkHourSegment(row) {
+  // take the vale from the span
+  var t = $(row).find("span").text().trim();
+  var textAreaEl = $(row).find("textarea");
 
+  //remove the previous class so the new class can take effect
+  if (textAreaEl.hasClass("present")) {
+    $(textAreaEl).removeClass("present");
+  } else if (textAreaEl.hasClass("future")) {
+    $(textAreaEl).removeClass("future");
+  } else if (textAreaEl.hasClass("past")) {
+    $(textAreaEl).removeClass("past");
+  }
+
+  //set it to a valid time according to the time in the span
+  var time = moment(t, "LT").set("hour", t);
+
+  //checks to see if the current time and time in the event segment are the same
+  if (moment().isSame(time, "hour")) {
+    //moment().isAfter(time)) {
+    $(textAreaEl).addClass("present");
+  } else if (moment().isBefore(time)) {
+    //checks to see if the current time is before the event segment time
+    $(textAreaEl).addClass("future");
+  } else if (moment().isAfter(time)) {
+    //checks to see if the current time is after the event segment time
+    $(textAreaEl).addClass("past");
+  }
 }
 
+//set a time interval to check and update the color of the textarea as time progresses
+setInterval(function () {
+  $(".container .row").each(function (index, el) {
+    console.log(el);
 
+    checkHourSegment(el);
+  });
+}, 1000 * 60 * 60);
 
-//when the save button is pressed the inputed 
-//text by the user is saved in the textarea
+//when the save button is pressed the input
+//text by the user is saved in the textarea and in local storage
 
-
-createHourSegments(09, 17);
+createHourSegments(09, 24);
